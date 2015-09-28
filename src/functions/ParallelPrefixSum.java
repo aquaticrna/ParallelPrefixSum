b6ddd8c585d0d1a09c3bb919fb5622eb566609a4
Kyle Langford <aquaticrna@gmail.com>
2015-09-27 23:13:34 -0700

    wrote the parallelization and comparison code

--- src/functions/ParallelPrefixSum.java
+++ src/functions/ParallelPrefixSum.java
@@ -8,36 +8,48 @@ import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicLongArray;
 
 
-
+//does prefix summing in parallel for monotonically increasing sets starting at 0
 public class ParallelPrefixSum {
 	public static void main(String[] args){
+		//base lining information to test speed
+		
+		long time = System.currentTimeMillis();//track the time
 		int[] array = new int[1000000];
+		//fill in the array
 		for(int x=0;x<array.length;x++){
 			array[x] = x;
 		}
-		long time = System.currentTimeMillis();
 		
+		//calculate the cumulative sum
 		long[] summed = new long[1000000];
 		summed[0] = 0;
 		for(int x=1;x<summed.length;x++){
 			summed[x] = array[x]+summed[x-1];
 		}
+		//bas time
 		long baseTime = System.currentTimeMillis()-time;
-		System.out.println(baseTime);
+		System.out.println("linear time = " + baseTime + "ms");
+		
+		//start timing again
 		time = System.currentTimeMillis();
+		
+		//set up our thread pool
 		ExecutorService exec = Executors.newCachedThreadPool();
+		
 		int quarter = array.length/4;
-		AtomicLongArray summed2 = new AtomicLongArray(1000000); 
+		AtomicLongArray summed2 = new AtomicLongArray(1000000);
 		List<Future<Integer>> futures = new ArrayList<Future<Integer>>();
+		
+		//spawns a thread and future object for each quarter of the list
 		for(int x=0;x<quarter*3;x+=quarter){
 			futures.add(exec.submit(new SubSum(summed2,x,x+quarter)));
 		}
-		
-		futures.add(exec.submit(new SubSum(summed2,quarter*3,summed2.length())));
-		futures.stream().forEach(f -> f.isDone());
+		futures.add(exec.submit(new SubSum(summed2,quarter*3,summed2.length())));//last quarter includes any remainder
+		futures.stream().forEach(f -> f.isDone());//wait for them to finish
 		exec.shutdown();
-		System.out.println((System.currentTimeMillis()-time));//check out guava
-		for(int x= 0;x<array.length;x++){
+		//error checking and time comparison
+		System.out.println("Parallel Time = " + (System.currentTimeMillis()-time) + "ms");//parallel processing becomes faster between n = 10 million and n = 50 million
+		for(int x= 0;x<summed.length;x++){
 			if (summed[x] !=summed2.get(x)){
 				System.out.println(x);
 				System.out.println("wrong calculation");
--- src/functions/SubSum.java
+++ src/functions/SubSum.java
@@ -8,17 +8,19 @@ public class SubSum implements Callable<Integer>{
 	int startIndex;
 	int endIndex;
 	
+	//takes in the array to be filled and the indices to fill, only works for sets that are monotonically increasing from 0 to
+	//n where n is the length of the array
 	public SubSum(AtomicLongArray array,int startIndex, int endIndex){
 		this.array = array;
 		this.startIndex = startIndex;
 		this.endIndex = endIndex;
 	}
-
+	
 	@Override
 	public Integer call() {
 		for(long x=startIndex;x<endIndex;x++){
-			long sum = x*(x+1)/2;
-			array.set((int) x, sum);
+			long sum = x*(x+1)/2;//triangles the number for the index
+			array.set((int) x, sum);//sets the index to the appropriate number
 		}
 		return 1;
 	}
